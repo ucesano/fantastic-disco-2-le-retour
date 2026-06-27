7c1ad1bada19f1acd820c2f1c254debf109dff32
ucesano <ucesano@etik.com>
2026-06-27 14:01:34 +0200

    Scatter.

--- main.cu
+++ main.cu
@@ -15,6 +15,7 @@
 #include <cmath>
 
 #include <mpi.h>
+#include <mpi-ext.h>
 #include <cuda_runtime.h>
 
 #ifdef __cplusplus
@@ -72,42 +73,87 @@ static inline int global_to_local(const int i, const int P) { return i / P; }
 
 static inline int local_to_global(const int l, const int p, const int P) { return l * P + p; }
 
-void sort_by_rank(std::vector<int>& I, std::vector<int>& J, std::vector<float>& val, std::vector<float>& X, int P)
+void pack_by_rank(std::vector<int>& I, std::vector<int>& J, std::vector<float>& val, int P)
 {
     const std::size_t nnz = I.size();
     if (J.size() != nnz || val.size() != nnz) {
-        std::cerr << "[sort_coo_by_rank] SIZE MISMATCH\n";
+        std::cerr << "[pack_by_rank] SIZE MISMATCH\n";
         return;
     }
 
-    // 1. perm = [0, 1, ..., nnz-1]
     std::vector<std::size_t> perm(nnz);
     std::iota(perm.begin(), perm.end(), 0);
 
-    // 2. order indices by owning rank; stable keeps rows ascending within a rank
     std::stable_sort(perm.begin(), perm.end(),
         [&](std::size_t a, std::size_t b) {
             return owner(I[a], P) < owner(I[b], P);
         });
 
-    // 3. apply the one permutation to all three arrays
-    std::vector<int>   I2(nnz);
-    std::vector<int>   J2(nnz);
+    std::vector<int>   I2(nnz), J2(nnz);
     std::vector<float> val2(nnz);
-    std::vector<float> X2(nnz);
-
     for (std::size_t k = 0; k < nnz; ++k) {
         I2[k]   = I[perm[k]];
         J2[k]   = J[perm[k]];
         val2[k] = val[perm[k]];
-        X2[k]   = X2[perm[k]];
     }
 
     I   = std::move(I2);
     J   = std::move(J2);
     val = std::move(val2);
-    X   = std::move(X2);
 }
+
+void pack_x_by_rank(std::vector<float>& X, int P)
+{
+    const int n = (int)X.size();   // = g_N
+    std::vector<int> xcount(P, 0), xdispl(P, 0);
+    for (int i = 0; i < n; ++i) xcount[i % P]++;
+    for (int r = 1; r < P; ++r) xdispl[r] = xdispl[r-1] + xcount[r-1];
+
+    std::vector<float> X2(n);
+    std::vector<int> cursor = xdispl;
+    for (int i = 0; i < n; ++i)
+        X2[cursor[i % P]++] = X[i];   // entry i (column i) -> rank i%P's block
+
+    X = std::move(X2);
+}
+
+void scatter_in_place(int g_nz, int g_N, int P, int rank, int*& d_I, int*& d_J, float*& d_val, float*& d_X, int nz, int N)
+{
+    // 1. counts + displs on rank 0 (needed for the Scatterv send side)
+    std::vector<int> ccount(P,0), cdispl(P,0), xcount(P,0), xdispl(P,0);
+    if (rank == 0)
+    {
+        std::vector<int> hI(g_nz);
+        cudaMemcpy(hI.data(), d_I, g_nz*sizeof(int), cudaMemcpyDeviceToHost);
+        for (int k = 0; k < g_nz; ++k) ccount[hI[k] % P]++;
+        for (int r = 1; r < P; ++r) cdispl[r] = cdispl[r-1] + ccount[r-1];
+        for (int r = 0; r < P; ++r) xcount[r] = (g_N - r + P - 1) / P;
+        for (int r = 1; r < P; ++r) xdispl[r] = xdispl[r-1] + xcount[r-1];
+    }
+
+    // 3. temporary recv buffers (sized by the known local counts)
+    int   *r_I, *r_J;
+    float *r_val, *r_X;
+    cudaMalloc(&r_I,   nz * sizeof(int));
+    cudaMalloc(&r_J,   nz * sizeof(int));
+    cudaMalloc(&r_val, nz * sizeof(float));
+    cudaMalloc(&r_X,   N  * sizeof(float));
+
+    // 4. scatter (device ptrs for data, host arrays for counts/displs)
+    MPI_Scatterv(d_I,   ccount.data(), cdispl.data(), MPI_INT,   r_I,   nz, MPI_INT,   0, MPI_COMM_WORLD);
+    MPI_Scatterv(d_J,   ccount.data(), cdispl.data(), MPI_INT,   r_J,   nz, MPI_INT,   0, MPI_COMM_WORLD);
+    MPI_Scatterv(d_val, ccount.data(), cdispl.data(), MPI_FLOAT, r_val, nz, MPI_FLOAT, 0, MPI_COMM_WORLD);
+    MPI_Scatterv(d_X,   xcount.data(), xdispl.data(), MPI_FLOAT, r_X,   N,  MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+    // 5. free rank 0's full arrays
+    if (rank == 0) {
+        cudaFree(d_I); cudaFree(d_J); cudaFree(d_val); cudaFree(d_X);
+    }
+
+    // 6. rebind caller pointers to the slices
+    d_I = r_I;  d_J = r_J;  d_val = r_val;  d_X = r_X;
+}
+
 
 #pragma endregion partitioning_helpers
 
@@ -164,7 +210,9 @@ inline void display_card_informations(const int devCount, std::ostream& os = std
 void print_coo_local(const std::vector<int>& I,
                      const std::vector<int>& J,
                      const std::vector<float>& val,
-                     int P, int rank = -1) {
+                     const std::vector<float>& X,
+                     int P, int rank = -1)
+{
     const std::size_t nnz = I.size();
     if (J.size() != nnz || val.size() != nnz) {
         std::cerr << "[print_coo_local] SIZE MISMATCH\n";
@@ -207,6 +255,46 @@ void print_coo_local(const std::vector<int>& I,
         if (k + 1 < nnz) std::cout << ", ";
     }
     std::cout << "]\n";
+
+    std::cout << "X     = [";
+    for (std::size_t k = 0; k < X.size(); ++k) {
+        std::cout << X[k];
+        if (k + 1 < X.size()) std::cout << ", ";
+    }
+    std::cout << "]\n";
+}
+
+void debug_print_device(const int* d_I, const int* d_J,
+                        const float* d_val, const float* d_X,
+                        int nz, int N, int rank)
+{
+    std::vector<int>   hI(nz), hJ(nz);
+    std::vector<float> hval(nz), hX(N);
+
+    cudaMemcpy(hI.data(),   d_I,   nz * sizeof(int),   cudaMemcpyDeviceToHost);
+    cudaMemcpy(hJ.data(),   d_J,   nz * sizeof(int),   cudaMemcpyDeviceToHost);
+    cudaMemcpy(hval.data(), d_val, nz * sizeof(float), cudaMemcpyDeviceToHost);
+    cudaMemcpy(hX.data(),   d_X,   N  * sizeof(float), cudaMemcpyDeviceToHost);
+
+    std::cout << "=== rank " << rank << " (nz=" << nz << ", N=" << N << ") ===\n";
+
+    std::cout << "I   = [";
+    for (int k = 0; k < nz; ++k) { std::cout << hI[k]; if (k+1 < nz) std::cout << ", "; }
+    std::cout << "]\n";
+
+    std::cout << "J   = [";
+    for (int k = 0; k < nz; ++k) { std::cout << hJ[k]; if (k+1 < nz) std::cout << ", "; }
+    std::cout << "]\n";
+
+    std::cout << "val = [";
+    for (int k = 0; k < nz; ++k) { std::cout << hval[k]; if (k+1 < nz) std::cout << ", "; }
+    std::cout << "]\n";
+
+    std::cout << "X   = [";
+    for (int i = 0; i < N; ++i) { std::cout << hX[i]; if (i+1 < N) std::cout << ", "; }
+    std::cout << "]\n";
+
+    std::cout.flush();
 }
 
 #pragma endregion debug
@@ -245,6 +333,11 @@ int main(int argc, char ** argv)
     int local_dev = p % dev_count;
     CHECK_CUDA(cudaSetDevice(local_dev));
 
+    #if defined(MPIX_CUDA_AWARE_SUPPORT) && MPIX_CUDA_AWARE_SUPPORT
+    if (p == 0 && !MPIX_Query_cuda_support())
+        fprintf(stderr, "WARNING: MPI is not CUDA-aware at runtime\n");
+    #endif
+
     // Executable must have a MTX file as arg.
     if (argc < 2)
     {
@@ -262,12 +355,21 @@ int main(int argc, char ** argv)
     int N  = 0;
     int nz = 0;
 
+    int owned_N = 0;
+
     std::vector<int> I;
     std::vector<int> J;
 
     std::vector<float> val;
     std::vector<float> X;
     std::vector<float> Y;
+
+    int * d_I = nullptr;
+    int * d_J = nullptr;
+
+    float * d_val = nullptr;
+    float * d_X   = nullptr;
+    float * d_Y   = nullptr;
 
     #pragma region loading_file
 
@@ -328,8 +430,8 @@ int main(int argc, char ** argv)
 
         mm_sort_coo(I.data(), J.data(), val.data(), g_nz);
 
-        X.reserve(g_N);
-        for (int i = 0; i < g_N; i++) X[i] =  static_cast<float>(genrand_real1() * 100);
+        X.resize(g_N);
+        for (int i = 0; i < g_N; i++) X[i] =  static_cast<float>(genrand_real1());
     }
 
     CHECK_MPI(MPI_Bcast(&g_M, 1, MPI_INT, 0, MPI_COMM_WORLD));
@@ -338,18 +440,73 @@ int main(int argc, char ** argv)
 
     #pragma endregion loading_file
 
+    if (p == 0) print_coo_local(I, J, val, X, P, p);
+
     #pragma region partitionning_matrix
+
+    M  = (g_M - p + P - 1) / P;
+    N  = g_N;
+
+    owned_N = (g_N - p + P - 1) / P;
 
     if (p == 0)
     {
-        sort_by_rank(I, J, val, X, P);
+        pack_by_rank(I, J, val, P);
+        pack_x_by_rank(X, P);
     }
+
+    nz = 0;
+    if (p == 0)
+    {
+        std::vector<int> ccount(P, 0);
+
+        for (int k = 0; k < g_nz; ++k)
+            ccount[owner(I[k], P)]++;
+
+        MPI_Scatter(ccount.data(), 1, MPI_INT, &nz, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+    else
+    {
+        MPI_Scatter(nullptr, 1, MPI_INT, &nz, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+
 
     #pragma endregion partitionning_matrix
 
-    if (p == 0) print_coo_local(I, J, val, P, p);
+    if (p == 0) print_coo_local(I, J, val, X, P, p);
+
+    #pragma region matrix_scatter
+
+    if (p == 0)
+    {
+        cudaMalloc(&d_I,   g_nz * sizeof(int));
+        cudaMalloc(&d_J,   g_nz * sizeof(int));
+        cudaMalloc(&d_val, g_nz * sizeof(float));
+        cudaMalloc(&d_X,   g_N  * sizeof(float));
+
+        cudaMemcpy(d_I,   I.data(),   g_nz * sizeof(int),   cudaMemcpyHostToDevice);
+        cudaMemcpy(d_J,   J.data(),   g_nz * sizeof(int),   cudaMemcpyHostToDevice);
+        cudaMemcpy(d_val, val.data(), g_nz * sizeof(float), cudaMemcpyHostToDevice);
+        cudaMemcpy(d_X,   X.data(),   g_N  * sizeof(float), cudaMemcpyHostToDevice);
+
+        cudaDeviceSynchronize();
+    }
+
+    scatter_in_place(g_nz, g_N, P, p, d_I, d_J, d_val, d_X, nz, owned_N);
+
+    debug_print_device(d_I, d_J, d_val, d_X, nz, owned_N, p);
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    #pragma endregion matrix_scatter
+
+    // if (p == 0) print_coo_local(I, J, val, P, p);
 
     #pragma region cleaning_up
+
+    CHECK_CUDA(cudaFree(d_I));
+    CHECK_CUDA(cudaFree(d_J));
+    CHECK_CUDA(cudaFree(d_val));
+    CHECK_CUDA(cudaFree(d_X));
 
     CHECK_MPI(MPI_Finalize());
 
